ff79ff24a64410a1e2c7fcdf54120c37114382e3
Fabricio-AE <fabricioae21@gmail.com>
2022-12-21 23:55:08 -0600

    Connection to master with one client

--- slave/main.c
+++ slave/main.c
@@ -1,18 +1,73 @@
+#include <arpa/inet.h> // inet_addr()
+#include <netdb.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h> // bzero()
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <unistd.h> // read(), write(), close()
+#define MAX 80
+#define PORT 8080
+#define SA struct sockaddr
 
-void initSocket();
+void func(int sockfd);
 
-int main(int argc, char** argv){
-    printf("Hola estoy aquí\n");
-    initSocket();
-    return 0;
+int main()
+{
+    int sockfd, connfd;
+    struct sockaddr_in servaddr, cli;
+
+    // socket create and verification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+    {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_port = htons(PORT);
+
+    // connect the client socket to server socket
+    if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) != 0)
+    {
+        printf("connection with the server failed...\n");
+        exit(0);
+    }
+    else
+        printf(" %i connected to the server..\n", sockfd);
+
+    // function for chat
+    func(sockfd);
+
+    // close the socket
+    close(sockfd);
 }
 
-void initSocket(){
-    while(1){
-        struct sockaddr_in addr;
-        
+void func(int sockfd)
+{
+    char buff[MAX];
+    int n;
+    for (;;)
+    {
+        bzero(buff, sizeof(buff));
+        printf("Enter the string : ");
+        n = 0;
+        while ((buff[n++] = getchar()) != '\n')
+            ;
+        write(sockfd, buff, sizeof(buff));
+        bzero(buff, sizeof(buff));
+        read(sockfd, buff, sizeof(buff));
+        printf("From Server : %s", buff);
+        if ((strncmp(buff, "exit", 4)) == 0)
+        {
+            printf("Client Exit...\n");
+            break;
+        }
     }
 }
